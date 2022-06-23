6f482e4d1ce725c37741645791c065b3c757f3c6
Floaterest <floaterest@outlook.jp>
2022-06-23 14:44:03 -0400

    refactor(hook,path)

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,6 +1,8 @@
 import * as path from 'path';
 import * as fs from 'fs';
 
+import { Type } from '$lib/types';
+
 import mime from 'mime';
 
 const argv = path.resolve(process.argv[process.argv.length - 1]);
@@ -10,22 +12,29 @@ export async function handle({ event, resolve }){
 	const pathname = event.url.pathname.replace(/\/__data\.json$/, '');
 	const cwd = path.join(root, pathname);
 	const stat = await fs.promises.lstat(cwd);
-	switch(true){
-		case stat.isFile():
-			return new Response(await fs.promises.readFile(cwd), {
-				headers: { 'Content-Type': mime.getType(cwd) + ';charset=utf-8', }
-			});
-		case stat.isDirectory():
-			const items = await fs.promises.readdir(cwd);
-			if(items.includes('index.html')){
-				return new Response(await fs.promises.readFile(path.join(cwd, 'index.html')), {
-					status: 200,
-					headers: {
-						'Content-Type': 'text/html;charset=utf-8'
-					}
+	let content: Buffer | string[];
+	// assume not dir => is file
+	const type = stat.isDirectory() ? Type.Folder : Type.File;
+	switch(type){
+		case Type.File:
+			const t = mime.getType(cwd);
+			content = await fs.promises.readFile(cwd);
+			if(t && t !== 'text/plain'){
+				return new Response(content, {
+					headers: { 'Content-Type': t + ';charset=utf-8', }
 				});
 			}
-			event.locals = { root, cwd, items };
+			break;
+		case Type.Folder:
+			content = await fs.promises.readdir(cwd);
+			const index = path.join(cwd, 'index.html');
+			if(fs.existsSync(index)){
+				return new Response(await fs.promises.readFile(index), {
+					headers: { 'Content-Type': 'text/html;charset=utf-8' }
+				});
+			}
+			break;
 	}
+	event.locals = { type, root, cwd, content };
 	return await resolve(event);
 }
--- src/routes/[...path].ts
+++ src/routes/[...path].ts
@@ -1,26 +1,31 @@
 import * as fs from 'fs';
 import * as path from 'path';
 
-export async function get({ locals: { root, cwd, items } }){
-	if(items){
-		const files = [], folders = [];
-		for(const item of items){
-			const stat = await fs.promises.lstat(path.join(cwd, item));
-			if(stat.isDirectory()){
-				folders.push(item);
-			}else if(stat.isFile()){
-				files.push(item);
+import { Type } from '$lib/types';
+
+export async function get({ locals: { type, root, cwd, content } }){
+	switch(type){
+		case Type.Folder:
+			const files = [], folders = [];
+			for(const item of content){
+				const stat = await fs.promises.lstat(path.join(cwd, item));
+				if(stat.isDirectory()){
+					folders.push(item);
+				}else if(stat.isFile()){
+					files.push(item);
+				}
 			}
-		}
-		const sep = path.sep;
-		let r = '';
-		cwd = cwd.replace(root, '').split(sep).filter(Boolean).map(dir => ({
-			dir,
-			cum: r += '/' + dir // cumulative path
-		}));
-		return { status: 200, body: { root, cwd, sep, folders, files } };
-	}else{
-		// I have no idea what to do here
-		return { status: 404 };
+			const sep = path.sep;
+			let r = '';
+			cwd = cwd.replace(root, '').split(sep).filter(Boolean).map(dir => ({
+				dir,
+				cum: r += '/' + dir // cumulative path
+			}));
+			return { body: { root, cwd, sep, folders, files } };
+		case Type.File:
+			return { body: { root, cwd, content } };
+		default:
+			// I have no idea what to do here
+			return { status: 404 };
 	}
 }
