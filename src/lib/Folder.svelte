09fcaa3829d03cd6063fb3928afb2aac88e92b6e
floaterest <56704092+floaterest@users.noreply.github.com>
2022-06-29 13:29:21 -0400

    feat(Folder) use `<a>` instead of `window.open`

--- src/lib/Folder.svelte
+++ src/lib/Folder.svelte
@@ -29,55 +29,58 @@
 		return { path, component };
 	});
 
-	function open(path, file: boolean){
+	function url(path, file: boolean){
 		path = href.endsWith('/') ? `${href}${path}` : `${href}/${path}`;
-		window.open(file ? path : path + '/', '_self');
+		return file ? path : path + '/';
 	}
 
-	function back(){
-		const path = href.replace(/\/[^/]*\/?$/, '');
-		window.open(path, '_self');
-	}
 </script>
 
 <div id="cwd">
-    <code on:click={()=>window.open('/','_self')}>{root}</code>
+    <code><a href="/" target="_self">{root}</a></code>
     {#each cwd as { dir, cum }}
-        <code on:click={()=>window.open(cum,'_self')}>{sep}{dir}</code>
+        <code><a href="{cum}" target="_self">{sep}{dir}</a></code>
     {/each}
 </div>
 <!-- icons from material icons / google fonts -->
 <ul>
     {#if pathname !== '/'}
-        <li on:click={back}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px"
-                 class="icon folder"
-                 viewBox="0 0 24 24" width="24px" fill="#000000">
-                <path d="M0 0h24v24H0z" fill="none"/>
-                <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
-            </svg>
-            ..
+        <!-- <li on:click={back}> -->
+        <li>
+            <a href="{href.replace(/\/[^/]*\/?$/, '')}">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                     class="icon folder"
+                     viewBox="0 0 24 24" width="24px" fill="#000000">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
+                </svg>
+                ..
+            </a>
         </li>
     {/if}
     {#each folders as path}
-        <li on:click={()=>open(path,false)}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px"
-                 class="icon folder"
-                 viewBox="0 0 24 24" width="24px" fill="#000000">
-                <path d="M0 0h24v24H0z" fill="none"/>
-                <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
-            </svg>
-            {path}
+        <li>
+            <a href="{url(path, false)}" target="_self">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                     class="icon folder"
+                     viewBox="0 0 24 24" width="24px" fill="#000000">
+                    <path d="M0 0h24v24H0z" fill="none"/>
+                    <path d="M10 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2h-8l-2-2z"/>
+                </svg>
+                {path}
+            </a>
         </li>
     {/each}
     {#each files as { path, component }}
-        <li on:click={()=>open(path,true)}>
-            <svg xmlns="http://www.w3.org/2000/svg" height="24px"
-                 class="icon file"
-                 viewBox="0 0 24 24" width="24px" fill="#000000">
-                <svelte:component this={component}/>
-            </svg>
-            {path}
+        <li>
+            <a href="{url(path, true)}" target="_self">
+                <svg xmlns="http://www.w3.org/2000/svg" height="24px"
+                     class="icon file"
+                     viewBox="0 0 24 24" width="24px" fill="#000000">
+                    <svelte:component this={component}/>
+                </svg>
+                {path}
+            </a>
         </li>
     {/each}
 </ul>
@@ -107,13 +110,15 @@
 
     ul
         padding: 0
-
         li
-            display: flex
             padding-left: 1em
-            align-items: center
-            cursor: pointer
-
             &:hover
                 background-color: var(--dark)
+    a
+        display: flex
+        align-items: center
+        cursor: pointer
+        color: unset
+        width: 100%
+        text-decoration: none
 </style>
