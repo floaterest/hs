5a71baea8e66231872e0f4dfbd74c1fac03c4b08
floaterest <56704092+floaterest@users.noreply.github.com>
2022-10-25 17:36:58 -0400

    init

---
+++ src/routes/[...path]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import '../../lib/app.sass';
+    import { page } from '$app/stores';
+    import type { Content } from '$lib/type';
+    import { Type } from '$lib/type';
+    import Folder from '$lib/Folder.svelte';
+
+    const { root, path, type, data } = $page.data as Content;
+</script>
+{#if type === Type.Folder}
+    <Folder {root} {path} {data}/>
+{/if}
