b17fe49b943515b8c923f40fa29488454a932c36
floaterest <56704092+floaterest@users.noreply.github.com>
2022-10-30 12:18:48 -0400

    feat: cwd works

--- src/routes/[...path]/+page.svelte
+++ src/routes/[...path]/+page.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
     import '$lib/app.sass';
     import { page } from '$app/stores';
-    import type { Content } from '$lib/type';
+    import type { Locals } from '$lib/type';
     import { Type } from '$lib/type';
     import Folder from '$lib/Folder.svelte';
 
-    const { root, type, data } = $page.data as Content;
+    const { type, data, cwd } = $page.data as Locals;
 </script>
 {#if type === Type.Folder}
-    <Folder {root} {data}/>
+    <Folder {cwd} {data}/>
 {:else}
     {#if type === Type.File}
         {@html data}
