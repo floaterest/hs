bb1b670c8b75fbc794bb6eb02a62d1726015ced9
floaterest <56704092+floaterest@users.noreply.github.com>
2023-02-25 19:57:05 -0500

    feat: file works

---
+++ src/hooks.server.ts
@@ -0,0 +1,53 @@
+import { join, resolve } from 'path';
+import type { Handle } from '@sveltejs/kit';
+import * as fs from 'fs/promises';
+import type { Entry } from '$lib/type';
+import { Type } from '$lib/type';
+import * as mime from 'mime/lite';
+
+mime.define({ 'text/plain': ['ts'] }, true);
+
+const html = 'text/html';
+const root = await (async(r: string) => await stat(r) ? r : process.cwd())(
+    resolve(process.argv[process.argv.length - 1])
+);
+
+const respond = async(path: string, type: string | null) => new Response(
+    await fs.readFile(path),
+    { headers: type ? { 'Content-Type': type + ';charset=utf-8' } : {} }
+);
+
+async function stat(path: string): Promise<Type>{
+    /// returns entry type of path
+    const st = await fs.stat(path).catch(_ => null);
+    if(st && st.isFile()) return Type.File;
+    if(st && st.isDirectory()) return Type.Folder;
+    return Type.DNE;
+}
+
+function sort(lhs: Entry, rhs: Entry): number{
+    /// sort entries (folder first)
+    if(lhs.type == rhs.type){
+        return lhs.path.localeCompare(rhs.path);
+    }else{
+        return lhs.type == Type.Folder ? -1 : 1;
+    }
+}
+
+export const handle: Handle = async({ event, resolve }) => {
+    const { url: { pathname } } = event;
+    const path = decodeURIComponent(join(root, pathname));
+    const type = await stat(path);
+    // let data;
+    switch(type){
+        case Type.File:
+            const ty = mime.getType(path);
+            return respond(path, ty);
+        case Type.Folder:
+            const index = join(path, 'index.html');
+            if(await stat(index) == Type.File){
+                return respond(index, html);
+            }
+    }
+    return resolve(event);
+};
